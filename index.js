d4a7c25b84343e977a100d21302baf69b602e036
Yvonne M Pham <yvonnersz@gmail.com>
2020-07-29 01:52:09 +0000

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,20 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  let hour = parseInt(time.split(":")[0]);
+
+  if (hour <= 12) {
+    return "Good Morning";
+  } else if (hour >= 17) {
+    return "Good Evening";
+  } else {
+    return "Good Afternoon";
+  }
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(string) {
+  document.body.querySelector('h1#greeting').innerText = string;
+}
